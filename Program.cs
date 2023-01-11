96635768f63e12559f7d26381091aead192a02a5
firsttoken <monikasharma12330@gmail.com>
2023-01-11 23:36:14 +0530

    UC1_3_integer

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace FindMaximumNumber
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            int output = FindMax.Find_max_int(45, 78, 199);
+            Console.WriteLine(output);
+
+        }
+    }
+}
+
