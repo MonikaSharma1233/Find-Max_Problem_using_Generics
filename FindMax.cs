b8090c0448a097f3214296b6ed9c11996999087b
firsttoken <monikasharma12330@gmail.com>
2023-01-11 23:44:09 +0530

    UC1_Find Max of Intergers

---
+++ FindMax.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMaximumNumber
+{
+    public class FindMax
+    {
+        public static int Find_max_int(int fvalue, int svalue, int tvalue)
+        {
+            if (fvalue.CompareTo(svalue) > 0 && fvalue.CompareTo(tvalue) > 0)
+            {
+                return fvalue;
+            }
+            if (svalue.CompareTo(fvalue) > 0 && svalue.CompareTo(tvalue) > 0)
+            {
+                return svalue;
+            }
+            else if (tvalue.CompareTo(fvalue) > 0 && tvalue.CompareTo(svalue) > 0)
+            {
+                return tvalue;
+            }
+            throw new Exception("FirstNumber,SecondNumber,ThirdNumber are Same");
+        }
+
+    }
+
+}
+
